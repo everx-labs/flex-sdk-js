e8e31669554c3150fc98a48b210dcdcf0f0aad08
Ekaterina Pantaz <52739957+elasticLove1@users.noreply.github.com>
2022-08-29 18:07:37 +0300

    remove trash

--- contracts/account-ex.d.ts
+++
@@ -1,8 +0,0 @@
-import { SignerOption } from "../flex";
-export declare type AccountOptionsEx = {
-    address: string;
-    signer?: SignerOption;
-} | {
-    signer: SignerOption;
-};
-//# sourceMappingURL=account-ex.d.ts.map
--- flex/web3/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Log, LogLevel } from "../../contracts/helpers";
-import { EvrAccounts } from "./accounts";
-import { EvrSigners, SignerOption } from "./signers";
-import { EverWallet, TransferOptions } from "./ever-wallet";
-import { EvrConfig, Evr } from "./evr";
-import { toUnits, uint256 } from "./utils";
-export { EvrSigners, EvrAccounts, Log, LogLevel, SignerOption, EverWallet, TransferOptions, Evr, EvrConfig, toUnits, uint256, };
-//# sourceMappingURL=index.d.ts.map
