b73fa00f38ff27dd27a234211cadabe4059aba6d
Ekaterina Pantaz <52739957+elasticLove1@users.noreply.github.com>
2022-08-29 12:06:34 +0300

    trader wallers

--- examples/delegate-funds.js
+++ examples/delegate-funds.js
@@ -15,27 +15,18 @@ const examples_1 = require("./examples");
     try {
         const flex = new flex_1.Flex(examples_1.EXAMPLES_FLEX_CONFIG);
         const clientAddress = examples_1.CONFIG.trader.client;
+        const traderId = examples_1.CONFIG.trader.id;
         yield flex_1.Trader.deploy(flex, {
             client: {
                 address: clientAddress,
                 signer: "everWallet",
             },
-            id: examples_1.CONFIG.trader.id,
+            id: traderId,
             name: "trader_1",
             pubkey: "162c6c708018da073729dd4a60118425dd917e44653383f1faed4d16b94af30b"
         });
-        let trader_ever_wallet = yield flex_1.Trader.deployEverWallet(flex, {
-            clientAddress: clientAddress,
-            everWallet: {
-                address: clientAddress,
-                signer: "everWallet",
-            },
-            tokens: 100,
-            traderId: examples_1.CONFIG.trader.id,
-            wrapperAddress: "0:c072805ae38d548d4abbaddf929659d37584117b63b0969eb3f812c6252b12fb",
-        });
-        console.log(`Trader EVER wallet address: ${trader_ever_wallet}`);
-        console.log(`Trader wallets: ${JSON.stringify(flex_1.Trader.queryWallets(flex, { clientAddress: clientAddress, traderId: examples_1.CONFIG.trader.id }))}`);
+        console.log(`Client address: ${clientAddress}`);
+        console.log(`Trader wallets: ${JSON.stringify(flex_1.Trader.queryWallets(flex, { clientAddress }))}`);
         yield flex.close();
     }
     catch (err) {
--- examples/delegate-funds.ts
+++ examples/delegate-funds.ts
@@ -6,32 +6,35 @@ import { CONFIG, EXAMPLES_FLEX_CONFIG } from "./examples";
     try {
         const flex = new Flex(EXAMPLES_FLEX_CONFIG);
         const clientAddress = CONFIG.trader.client;
+        const traderId = CONFIG.trader.id;
         
         await Trader.deploy(flex, {
             client: {
                 address: clientAddress,
                 signer: "everWallet",
             },
-            id: CONFIG.trader.id,
+            id: traderId,
             name: "trader_1",
             pubkey: "162c6c708018da073729dd4a60118425dd917e44653383f1faed4d16b94af30b" //await flex.evr.signers.resolvePublicKey(CONFIG.trader.signer),
         });
 
         //{ pubkey: "0xa51a2ccb21eddfda9069aabc76faba5840f361b7a0c6eb51c925af44156a2802", owner: "0:d807caf6df3a7c2bb0b64915613eca9d8f17ca1de0b938dfdcbb9b4ff30c4526", evers: 15000000000, keepEvers: 12000000000 }
 
-        let trader_ever_wallet =  await Trader.deployEverWallet(flex, {
+       /* let trader_ever_wallet =  await Trader.deployEverWallet(flex, {
             clientAddress: clientAddress,
             everWallet: {
-                address: clientAddress,
+                address: "0:d807caf6df3a7c2bb0b64915613eca9d8f17ca1de0b938dfdcbb9b4ff30c4526",
                 signer: "everWallet",
             },
             tokens: 100,
-            traderId: CONFIG.trader.id,
+            traderId: traderId,
             wrapperAddress: "0:c072805ae38d548d4abbaddf929659d37584117b63b0969eb3f812c6252b12fb",
         });
 
         console.log(`Trader EVER wallet address: ${trader_ever_wallet}`);
-        console.log(`Trader wallets: ${JSON.stringify(Trader.queryWallets(flex, {clientAddress: clientAddress, traderId: CONFIG.trader.id}))}`);
+        */
+        console.log(`Client address: ${clientAddress}`);
+        console.log(`Trader wallets: ${JSON.stringify(Trader.queryWallets(flex, {clientAddress: clientAddress}))}`);
 
         await flex.close();
     } catch (err) {
