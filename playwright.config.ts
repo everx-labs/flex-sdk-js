99a64a18203125810f03cc2b637f88d41bd037cb
Sergei Voronezhskii <sergei.v@tonlabs.io>
2022-12-30 11:33:57 +0300

    add reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,12 @@ const TIMEOUT = 30 * 60 * 1000;
 const config: PlaywrightTestConfig = {
     timeout: 30 * 60 * 1000,
     workers: 1,
-    maxFailures: 1,
+    maxFailures: 100,
+    reporter: [
+        ["list"],
+        ["junit", { outputFile: "./integration-test/test-results/junit.xml" }],
+        ['allure-playwright', { outputFolder: './integration-test/test-results', suiteTitle: false }]
+    ],
     projects: [
         {
             name: "unit",
